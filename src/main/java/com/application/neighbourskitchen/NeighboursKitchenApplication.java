320fea128ad8a082e281bcb39e9bf3e64478ad05
Stylianos Andreolas <35779815+Divinesteel@users.noreply.github.com>
2021-11-16 19:25:24 +0200

    init

---
+++ src/main/java/com/application/neighbourskitchen/NeighboursKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.application.neighbourskitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighboursKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighboursKitchenApplication.class, args);
+	}
+
+}

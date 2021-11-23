7f427e7cffbf26cf05166147bdfaefa28cd08314
Stylianos Andreolas <35779815+Divinesteel@users.noreply.github.com>
2021-11-23 14:32:45 +0200

    logout

--- src/main/java/com/application/neighbourskitchen/configuration/SecurityConfiguration.java
+++ src/main/java/com/application/neighbourskitchen/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.application.neighbourskitchen.configuration;
 
 import com.application.neighbourskitchen.services.DatabaseUserDetailPasswordService;
 import com.application.neighbourskitchen.controller.DatabaseUserDetailsService;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,8 +11,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -43,16 +53,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-            csrf()
-            .disable()
+        http
+                .csrf()
+                .disable()
             .authorizeRequests()
-            .antMatchers("/registration")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/userLogout").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Override
---
+++ src/main/java/com/application/neighbourskitchen/controller/LogoutController.java
@@ -0,0 +1,33 @@
+package com.application.neighbourskitchen.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LogoutController {
+
+    @PostMapping("/logout")
+    @ResponseBody
+    public String logoutDo(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session= request.getSession(false);
+        SecurityContextHolder.clearContext();
+        session= request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+        for(Cookie cookie : request.getCookies()) {
+            cookie.setMaxAge(0);
+        }
+
+        return "logout";
+    }
+}
